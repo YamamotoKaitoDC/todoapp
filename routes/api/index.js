f27db837fd29a551b419ab6171a848a24d97b27f
YamamotoKaitoDC <k.yamamoto@g.dreamcareer.co.jp>
2022-04-22 15:41:18 +0900

    first commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -31,4 +31,23 @@ router.get("/tasks", async function (req, res, next) {
   res.send(getTasks);
 });
 
+//　タスクを1件取得するルーティング
+router.get("/tasks/:id", async function (req, res, next) {
+  const getTasksId = await tasks.getTasksId(req.params.id);
+  res.send(getTasksId);
+});
+
+//　タスクを1件更新するルーティング
+router.patch("/tasks/:id", async function (req, res, next) {
+  const patchTasksId = await tasks.patchTasksId(req.params.id, req.body);
+  res.send(patchTasksId);
+});
+
+//　タスク一覧を削除するルーティング
+router.delete("/tasks/:id", async function (req, res, next) {
+  const deleteTasksId = await tasks.deleteTasksId(req.params.id);
+  res.send(deleteTasksId);
+});
+
+
 module.exports = router;
--- src/tasks.js
+++ src/tasks.js
@@ -2,6 +2,7 @@
 
 const mysql = require("mysql2/promise");
 const config = require("../config.js");
+const { patch } = require("../routes/api/index.js");
 
 /**
 * タスクを新規登録する　API
@@ -14,11 +15,11 @@ postTasks = async function (body) {
       connection = await mysql.createConnection(config.dbSetting);
       //ここにSQLを記述する
       const sql =
-        "INSERT INTO todoapp.t_task (task_name, deadline, category_id) VALUES (?,?,?);";
-      let d = [body.taskName, body.deadline, body.category];
+        "INSERT INTO todoapp.t_task (task_name, deadline, category_id, priority_id) VALUES (?,?,?,?);";
+      let d = [body.taskName, body.deadline, body.category, body.priority];
       const [rows, fields] = await connection.query(sql, d);
 
-       console.log(rows);
+      // console.log(rows);
       return rows;
     } catch (err) {
         console.log(err);
@@ -29,14 +30,14 @@ postTasks = async function (body) {
 exports.postTasks = postTasks;
 
 
-
+//　タスクを一覧取得する
 getTasks = async function () {
     let connection = null;
     try {
         connection = await mysql.createConnection(config.dbSetting);
         //　ここにSQLを記述する
       const sql = 
-        "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id;";
+        "SELECT * FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id LEFT JOIN t_priority ON t_task.priority_id = t_priority.id;";
       const [rows, fields] = await connection.query(sql);
         return rows;
     } catch (err) {
@@ -46,4 +47,73 @@ getTasks = async function () {
     }
 };
 exports.getTasks = getTasks;
+
+
+//　タスクを一件取得するAPI
+getTasksId = async function (id) {
+    let connection = null;
+    try {
+        connection = await mysql.createConnection(config.dbSetting);
+        //　ここにSQLを記述する
+        const sql =
+          "SELECT * FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id LEFT JOIN t_priority ON t_task.priority_id = t_priority.id WHERE t_task.id = ?;";
+        let d = [id];
+        const [rows, fields] = await connection.query(sql,d);
+        return rows;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        connection.end();
+    }
+};
+exports.getTasksId = getTasksId;
+
+
+//　タスクを一件更新するAPI
+patchTasksId = async function (id, body) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    //　ここにSQLを記述する
+    const sql = 
+      "UPDATE t_task SET task_name=?, deadline=?, category_id=?, task_status=?, updated_at=?,  priority_id=? WHERE id=?;";
+      let d = [
+        body.taskName,
+        body.deadline,
+        body.category,
+        body.status,
+        new Date(),
+        body.priority,
+        id,
+      ];
+      const [rows, fields] = await connection.query(sql, d);
+      return rows;
+  } catch (err) {
+      console.log(err);
+  } finally {
+      connection.end();
+  }
+};
+exports.patchTasksId = patchTasksId;
+
+
+//　タスクを一件削除する
+deleteTasksId = async function (id) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    //ここにSQLを記述する
+    const sql = "DELETE from t_task WHERE id = ?;";
+    let d = [id];
+    const [rows, fields] = await connection.query(sql, d);
+
+    // console.log(rows);
+    return rows;
+  } catch (err) {
+      console.log(err);
+  } finally {
+      connection.end();
+  }
+};
+exports.deleteTasksId = deleteTasksId;
 
